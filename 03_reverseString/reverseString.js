6d453db0a1347351a4f50a99906dff4996950f80
Kunal <100947485+heyyitskk@users.noreply.github.com>
2023-07-28 23:35:36 +0530

    3rdDone

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let r = '';
+    for(const ch of string){
+        r = ch + r;
+    }
+    return r;
 };
 
 // Do not edit below this line
