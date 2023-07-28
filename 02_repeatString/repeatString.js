bc6a42545e9e96380fadc9a5d6de1d7e0c903868
Kunal <100947485+heyyitskk@users.noreply.github.com>
2023-07-28 23:21:05 +0530

    2ndDone

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let r = '';
+    if(n < 0)
+        return "ERROR";
+    while(n > 0){
+        r += string;
+        n--;
+    }
+    return r;
 };
 
 // Do not edit below this line
