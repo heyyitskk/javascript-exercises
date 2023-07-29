9141691a268f5ace57516d407cb5544eb99b79b6
Kunal <100947485+heyyitskk@users.noreply.github.com>
2023-07-29 11:35:44 +0530

    5thDone

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(i, n) {
+    let sum = 0;
+    if( typeof i != "number" || typeof n != "number" || i < 0 || n < 0)
+        return "ERROR";
+    else if(i > n){
+        let temp = i;
+        i = n;
+        n = temp;
+    }
+    while(i <= n){
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
