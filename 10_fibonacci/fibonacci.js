a1bade53f9501367b98932593461f9aa972edeb6
Kunal <100947485+heyyitskk@users.noreply.github.com>
2023-07-29 13:20:38 +0530

    10thDone

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function fib(n) {
+    n = parseInt(n);
+    if (n < 0)
+        return "OOPS";
+    if (n < 2)
+        return n;
+    return (fib(n-1) + fib(n-2));
 };
 
 // Do not edit below this line
