5a1a74c24ac4e887fd0c49b516f3d5c956eab235
Kunal <100947485+heyyitskk@users.noreply.github.com>
2023-07-29 13:08:24 +0530

    9thDone

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+ const text = string.toLowerCase();
+ const onlyA = text.replace(/[^a-z0-9]/g, '');
+ const reverse = onlyA.split("").reverse().join("");
+ if(reverse == onlyA)
+    return true;
+ return false;
 };
 
 // Do not edit below this line
